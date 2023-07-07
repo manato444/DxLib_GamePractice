6a01cb3b83aa0940fdfaec31bba51a4cb15cd11a
manato444 <138412544+manato444@users.noreply.github.com>
2023-07-07 11:34:03 +0900

    Match3

---
+++ GameProgramming/マッチ3/AppMain.cpp
@@ -0,0 +1,24 @@
+#include "DxLib.h"
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_ LPSTR lpCmdLine,_In_ int nShowCmd)
+{
+
+ChangeWindowMode(TRUE);
+
+
+if(DxLib_Init() ==-1)
+{
+ return -1;
+}
+
+ 
+
+WaitKey();
+
+  
+  DxLib_End();
+
+
+ return 0;
+}
